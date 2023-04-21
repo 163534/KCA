8c8801337eee576a22b5794c88aebe7506477d53
Harvie Hutchinson <163534@chrome.escg.ac.uk>
2023-04-21 16:05:31 +0100

    v0.2

---
+++ Assets/LadderScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Player");
+        }
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,38 +1,132 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Search;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     Vector3 velocity;
+    LayerMask groundLayerMask;
 
     public float speed = 6f;
+    float jForce = 6f;
+    float gravity = -10.8f;
+
+    float horizontal;
+    float vertical;
+
+
     public Transform cam;
     [SerializeField]
     private Transform target;
-
+    public Actions state;
+    bool grounded;
+    bool Jumping;
+    //bool climbing;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     private void Start()
     {
+        state = Actions.Idle;
+        groundLayerMask = LayerMask.GetMask("Ground");
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
     void Update()
     {
-        Movement();
-        Jump();
+        
+        DoLogic();
     }
-    void FixedUpdate()
+    private void LateUpdate()
     {
+        
+
+
     }
+
+    void DoLogic()
+    {
+        if(state == Actions.Idle)
+        {
+            Idle();
+            CursorStates();
+            print("Idling");
+            CheckForJump();
+            ApplyGravity();
+        }
+        if(state == Actions.Move)
+        {
+            Movement();
+            CursorStates();
+            print("moving");
+            CheckForJump();
+            ApplyGravity();
+        }
+        if(state == Actions.Jump)
+        {
+            Movement();
+            CheckForLanding();
+            CursorStates();
+            print("jumping");
+            ApplyGravity();
+        }
+        if(state == Actions.Climb)
+        {
+            Climbing();
+            CursorStates();
+            print("Climbing");
+        }
+
+       // print("state=" + state);
+       // print("grounded=" + grounded );
+    }
+    void Idle()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0 && !grounded)
+        {
+            state = Actions.Move;
+        }
+       
+        
+    }
+
+    void ApplyGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void CheckForJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) ) //&& transform.position.y >= 1.1f)
+        {
+            velocity.y = jForce;
+            state = Actions.Jump;
+            grounded = false;
+        }
+    }
+
+    void CheckForLanding()
+    {
+        if (grounded)
+        {
+            state = Actions.Idle;
+        }
+
+    }
+
+
     void Movement()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
 
+        
 
         // stores the Horizontal and Vertical input as a direction i.e W,A,S and D = forward, back, left and right //
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; // normalized insures speed doesn't change if moving diagonally //
@@ -52,33 +146,52 @@ public class PlayerMovement : MonoBehaviour
             controller.Move(moveDir.normalized * speed * Time.deltaTime); // using the character controller component move the object in the input's direction by speed //
         }
         
-    }
-    void Jump()
-    {
-        float jForce = 6f;
-        float gravity = -9.8f;
-
-                
-
-        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y >= 1.1f)
+        if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
         {
-            velocity.y = jForce;
+            state = Actions.Idle;
+        }
+
+    }
+  
+    void Climbing()
+    {
+       /* if (climbing)
+        {
+            print("climbing up");
+            Vector3 climb = new Vector3(0f, 2f, 0f);
+            controller.Move(climb * Time.deltaTime * speed);
+        }*/
+    }
+    void CursorStates()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    private void OnTriggerStay(Collider col)
+    {
+        if(col.CompareTag("Climb"))
+        {
+            print("climbing");
+           
         }
         else
         {
-            velocity.y += gravity * Time.deltaTime;
+            print("no longer climbing");
+           
         }
-        controller.Move(velocity * Time.deltaTime);
     }
-    void LookAt()
+    void OnControllerColliderHit(ControllerColliderHit col)
     {
-        transform.LookAt(target);
-    }
-    private void OnCollisionStay(Collision col)
-    {
-        if(col.gameObject.tag == "Climb")
+        
+
+        if (col.gameObject.tag == "Floor")
         {
-            Debug.Log("climbing");
+            grounded = true;
+            print("landed on " + col.gameObject.tag);
+
+
         }
     }
 }
